d8e9050c2fe4bc36853a9e7b30c278dde6a333ce
Bart Machielsen <bartmachielsen@gmail.com>
2016-12-19 12:53:20 +0100

    added testdriver for bing maps wrapper

---
+++ VVVOnTheWay/VVVOnTheWay/BingMapsWrapper/BingMapsWrapperTestDriver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVOnTheWay.BingMapsWrapper
+{
+    static class BingMapsWrapperTestDriver
+    {
+        public static async void TestBingMapsWrapper()
+        {
+            System.Diagnostics.Debug.WriteLine("Getting current location to check!");
+            var location = await LocationSystem.BingMapsWrapper.GetCurrentPosition();
+            System.Diagnostics.Debug.WriteLine($"Current location is\n" +
+                                               $"city: {location.CivicAddress.City}\n" +
+                                               $"postalcode: {location.CivicAddress.PostalCode}\n" +
+                                               $"country:{location.CivicAddress.Country}\n" +
+                                               $"state:{location.CivicAddress.State}\n" +
+                                               $"coordinate data:\n" +
+                                               $"latitude:{location.Coordinate.Point.Position.Latitude}\n" +
+                                               $"Longitude:{location.Coordinate.Point.Position.Longitude}\n" +
+                                               $"Altitude:{location.Coordinate.Point.Position.Altitude}\n" +
+                                               $"Speed:{location.Coordinate.Speed}\n" +
+                                               $"Accuracy:{location.Coordinate.Accuracy}\n" +
+                                               $"Heading:{location.Coordinate.Heading}\n" +
+                                               $"Timestamp{location.Coordinate.Timestamp}");
+
+
+            \
+        }
+    }
+}
